67b53ae43a76e3014b03e4328a35b4d864f25f5d
HommerCLOD <hommerclod@gmail.com>
2020-12-18 17:22:33 +0200

    Добавлено css. фльтри

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import {RegistrationModule} from './modules/registration/registration.module';
 import {MainRoutingModule} from './modules/main-page/main-routing.module';
 import { LoginComponent } from './modules/login/login-components/login.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+//тут всі необхіні імпорти для використання ангулярівських методів таблиць і подібного
 
 @NgModule({
   declarations: [
@@ -21,7 +29,12 @@ import {ReactiveFormsModule} from '@angular/forms';
     AppRoutingModule,
     MainRoutingModule,
     RegistrationModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatOptionModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
