b823826e53f9b9f73cbd11f7dd185ec10b9a4b08
HommerCLOD <hommerclod@gmail.com>
2021-03-15 16:42:53 +0200

    Добавлено сторінку продукта + блок з коментами Замінено іконки і добавлено оптиізовані (в різних розширеннях)

--- src/app/modules/product/components/product.component.ts
+++ src/app/modules/product/components/product.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 
+interface Rating {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
+
 export class ProductComponent implements OnInit {
+  stars: Rating[] = [
+    {value: '1', viewValue: '1 Star'},
+    {value: '2', viewValue: '2 Stars'},
+    {value: '3', viewValue: '3 Stars'},
+    {value: '4', viewValue: '4 Stars'},
+    {value: '5', viewValue: '5 Stars'}
+  ];
 
   constructor(private router: Router) {
   }
--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './components/product.component';
+import {MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatOptionModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [ProductComponent],
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatOptionModule,
+    MatSelectModule,
+    FormsModule
   ]
 })
 export class ProductModule { }
