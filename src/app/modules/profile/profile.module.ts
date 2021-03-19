ae87e20fba6bbb4a91953ad8fad5b58993d9d967
NotQuestions <kipen.vasyl@gmail.com>
2021-03-19 17:24:00 +0200

    - Slider Done

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import {ProfileComponent} from './components/profile.component';
 import {ProfileRoutingModule} from './profile-routing.module';
 import {MatButtonToggleGroup, MatButtonToggleModule} from '@angular/material/button-toggle';
-import { AccountInfoComponent } from './childe.component/account-info/account-info.component';
+// import { AccountInfoComponent } from './childe.component/account-info/account-info.component';
 import { AccountSecuritySettingsComponent } from './childe.component/account-settings/account-security-settings.component';
+import {AccountInfoComponent} from './childe.component/account-info/account-info.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent, AccountInfoComponent, AccountSecuritySettingsComponent],
+  declarations: [
+    ProfileComponent,
+    AccountInfoComponent,
+    AccountSecuritySettingsComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule,
