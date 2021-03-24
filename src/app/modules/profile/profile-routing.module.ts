91cdefd0bec806c8a74d057e736ae1dc86797fac
Serhii <hommerclod@gmail.com>
2021-03-24 15:59:07 +0200

    обавлена панель адміна ( в доробці) Добавлена кнопка відправлення комента Добавлено історію покупок юзера

--- src/app/modules/profile/profile-routing.module.ts
+++ src/app/modules/profile/profile-routing.module.ts
@@ -3,13 +3,15 @@ import {NgModule} from '@angular/core';
 import {ProfileComponent} from './components/profile.component';
 import {AccountInfoComponent} from './childe.component/account-info/account-info.component';
 import {AccountSecuritySettingsComponent} from './childe.component/account-settings/account-security-settings.component';
+import {AccountHistoryComponent} from './childe.component/account-history/account-history.component';
 
 
 const routes: Routes = [
   {
     path: '', component: ProfileComponent, children: [
       {path: 'account_info', component: AccountInfoComponent},
-      {path: 'account_security_settings', component: AccountSecuritySettingsComponent}
+      {path: 'account_security_settings', component: AccountSecuritySettingsComponent},
+      {path: 'account_history', component: AccountHistoryComponent}
     ]
   }
 ];
--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -5,11 +5,12 @@ import {ProfileRoutingModule} from './profile-routing.module';
 import { MatButtonToggleModule} from '@angular/material/button-toggle';
 import { AccountSecuritySettingsComponent } from './childe.component/account-settings/account-security-settings.component';
 import {AccountInfoComponent} from './childe.component/account-info/account-info.component';
+import { AccountHistoryComponent } from './childe.component/account-history/account-history.component';
 
 
 
 @NgModule({
-  declarations: [ProfileComponent, AccountInfoComponent, AccountSecuritySettingsComponent],
+  declarations: [ProfileComponent, AccountInfoComponent, AccountSecuritySettingsComponent, AccountHistoryComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule,
