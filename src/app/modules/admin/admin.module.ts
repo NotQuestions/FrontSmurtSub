ec71e96c46705f6aca7cc77baf1fdce6970b95d7
Serhii <hommerclod@gmail.com>
2021-04-01 16:24:05 +0300

    Дороблела панель адміна. Добавлені розкриваючі плашки і змінені кольори

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AdminComponent} from './components/admin.component';
 import {AdminRoutingModule} from './admin-routing.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {DemoMaterialModule} from './components/materil-module';
 
 
 
@@ -9,7 +11,9 @@ import {AdminRoutingModule} from './admin-routing.module';
   declarations: [AdminComponent],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatFormFieldModule,
+    DemoMaterialModule
   ]
 })
 export class AdminModule { }
